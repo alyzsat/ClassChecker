d50162f6392ad6ba35255cbce96d2bc6ebbca6d7
Alyzsa <48876765+alyzsat@users.noreply.github.com>
2019-09-26 13:52:50 -0700

    Update GUI.py
    Opens a window titled ClassChecker
    Adjusts window size to screen resolution

--- GUI.py
+++ GUI.py
@@ -1,5 +1,41 @@
 from web_scraping import CoursePage
+from PyQt5.QtWidgets import QWidget, QApplication, QDesktopWidget
+import sys
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'ClassChecker'
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle(self.title)
+        self.set_dimensions()
+        self.show()
+
+    def set_dimensions(self):
+        """Sets the size of the window in proportion to the screen resolution"""
+        # Get screen resolution
+        desktop_size = QDesktopWidget().screenGeometry()
+        h = desktop_size.height()
+        w = desktop_size.width()
+
+        # Determine screen dimensions
+        self.height = h / 1.5
+        self.width = w / 1.5
+
+        # Set the size of the window
+        self.setGeometry((w-self.width)/2, (h-self.height)/2, self.width, self.height)
+
+
+
 if __name__ == '__main__':
-    cp = CoursePage("2019", "92", "34414")
-    cp.refresh()
-    print(cp.get_restrictions())
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
+
+
+    # cp = CoursePage("2019", "92", "34414")
+    # cp.refresh()
+    # print(cp.get_restrictions())
